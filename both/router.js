fbcf7e2a92a95afa61ec40483856bd1b31412f74
Joey Arnold <jandres@ualberta.ca>
2016-01-30 23:41:10 -0700

    button demo

--- both/router.js
+++ both/router.js
@@ -15,6 +15,7 @@ Router.map(function() {
   this.route('index', {path: '/'});
   this.route('actionSheet');
   this.route('backdrop');
+  this.route('buttons');
   this.route('content');
   this.route('forms', {
     data: function () {
